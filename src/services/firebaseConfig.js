eaad57874c41b470074347b631d06d6ea238d1d1
Santiago Pantín <santi240299@gmail.com>
2025-12-08 18:16:26 -0300

    Expone firebaseConfig para corrección y exporta Firestore

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBcS4gcVIRP2KEw0XrPqqNteH69rLGmVNA",
+  authDomain: "velavita-ebfba.firebaseapp.com",
+  projectId: "velavita-ebfba",
+  storageBucket: "velavita-ebfba.firebasestorage.app",
+  messagingSenderId: "751793458206",
+  appId: "1:751793458206:web:b02ea97df9bba0e1e3720f"
 };
 
 const app = initializeApp(firebaseConfig);
